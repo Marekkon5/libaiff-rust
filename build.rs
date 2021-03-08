a05d6da1ad962ce152f165167264d56af58d8158
Marekkon5 <marekkon5@yahoo.com>
2021-03-08 22:11:59 +0100

    Removed header files, don't regenerate bindings

--- build.rs
+++ build.rs
@@ -3,6 +3,13 @@ use std::path::PathBuf;
 fn main() {
     //Link to libaiff
     println!("cargo:rustc-link-lib=aiff");
+
+    //Don't regenerate
+    let path = PathBuf::from("src").join("bindings.rs");
+    if path.exists() {
+        return;
+    }
+
     //Recompile on wrapper change
     println!("cargo:rerun-if-changed=wrapper.h");
 
@@ -13,7 +20,6 @@ fn main() {
         .expect("Error generating bindings!");
 
     //Write
-    let path = PathBuf::from("src").join("bindings.rs");
     bindings
         .write_to_file(path)
         .expect("Error writing bindings!");
